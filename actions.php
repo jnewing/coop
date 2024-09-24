c76835bbc7b0c3d9374ccbeeff4d4cd17016461f
malachi@brunskill.ca <malachi@brunskill.ca>
2024-09-24 15:22:43 -0400

    Done

--- actions.php
+++ actions.php
@@ -9,13 +9,17 @@ print 'Global: $_GET varaible' . "\r\n";
 print print_r($_GET, true);
 print "\r\n";
 
-print 'Global: $_POST variable' . "\r\n";
-print print_r($_POST, true);
-print "\r\n";
+// print 'Global: $_POST variable' . "\r\n";
+// print print_r($_POST, true);
+// print "\r\n";
 
 // print 'Global: $_SERVER variable' . "\r\n";
 // print print_r($_SERVER, true);
 // print "\r\n";
+
+print strtoupper($_GET["field1"]) . " " . $_GET["some_email"] . " " . $_GET['more_stuff'];
+print "\r\n";
+print strtoupper($_GET['field1']) . " {$_GET['some_email']} {$_GET['more_stuff']}";
 
 ?>
 </pre>
--- page3.php
+++ page3.php
@@ -1,27 +1,36 @@
 <!doctype html>
+
+
 <html>
 <head>
     <title>This is the title of the webpage!</title>
 </head>
 <body>
-    <h1>Example Form Page</h1>
+    <h1>Basic Form</h1>
     <p>This is an example paragraph. Anything in the <strong>body</strong> tag will appear on the page, 
     just like this <strong>p</strong> tag and its contents.</p>
 
-    <form action="actions.php">
-        <label>Field 1:</label>
-        <input type="text" name="field1" />
+    <form action="" method="post">
+        <label><strong>Phone #:</strong></label>
+        <input type="phone" name="usr_phone" />
         <br /><br />
 
-        <label>Email Field</label>
-        <input type="email" name="some_email" />
+        <label><strong>Email Field</strong></label>
+        <input type="email" name="usr_email" />
         <br /><br />
 
-        <label>Text Area</label><br />
-        <textarea placeholder="put some stuff here." name="more_stuff"></textarea>
+        <label><strong>Message</strong></label><br />
+        <textarea placeholder="Type message here" name="usr_message"></textarea>
         <br /><br />
 
-        <button type="submit">Process...</button>
+        <button type="submit" name="done">Done</button>
     </form>
+    <?php
+if(isset($_POST['done'])){
+  $input = $_POST['usr_phone'] . $_POST['usr_email'] . $_POST['usr_message'];
+  $message = "Success! You entered: " . $input;
+     print($message); 
+}
+?>
 </body>
 </html>
